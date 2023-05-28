60723b00dfaa5569d5095fa3a418d065315dc8b1
nisaachar <thhat.guy.is@gmail.com>
2023-05-28 11:47:55 -0400

    ipynb files

--- Computer Vision/HW1/opencv.py
+++ Computer Vision/HW1/opencv.py
@@ -1,58 +1,59 @@
 import cv2
 import numpy as np
+apple = cv2.imread('apple.png')
+orange = cv2.imread('orange.png')
+print(apple.shape)
+print(orange.shape)
+apple_orange = np.hstack((apple[:, :256], orange[:, 256:]))
 
-def laplacian_pyramid(image, num_levels):
-    pyramid = [image]
-    for i in range(num_levels):
-        image = cv2.pyrDown(image)
-        pyramid.append(image)
-    return pyramid
-
-def reconstruct_laplacian_pyramid(pyramid):
-    num_levels = len(pyramid) - 1
-    reconstructed = pyramid[num_levels]
-    for i in range(num_levels, 0, -1):
-        expanded = cv2.pyrUp(reconstructed)
-        expanded = cv2.resize(expanded, pyramid[i - 1].shape[::-1])
-        reconstructed = cv2.add(pyramid[i - 1], expanded)
-    return reconstructed
-
-def blend_images(image1, image2, mask, num_levels):
-    # Create Laplacian pyramids for each image
-    image1_pyramid = laplacian_pyramid(image1, num_levels)
-    image2_pyramid = laplacian_pyramid(image2, num_levels)
-
-    # Create blended pyramid using the mask
-    blended_pyramid = []
-    for i in range(num_levels):
-        height, width = image1_pyramid[i].shape[:2]
-        mask_resized = cv2.resize(mask, (width, height))
-        mask_normalized = mask_resized / 255.0
-
-        blended_level = image1_pyramid[i] * (1 - mask_normalized) + image2_pyramid[i] * mask_normalized
-        blended_pyramid.append(blended_level)
-
-    # Reconstruct the blended image from the pyramid
-    blended_image = reconstruct_laplacian_pyramid(blended_pyramid)
-
-    return blended_image
+# generate Gaussian pyramid for apple
+apple_copy = apple.copy()
+gp_apple = [apple_copy]
+for i in range(6):
+    apple_copy = cv2.pyrDown(apple_copy)
+    gp_apple.append(apple_copy)
 
 
+# generate Gaussian pyramid for orange
+orange_copy = orange.copy()
+gp_orange = [orange_copy]
+for i in range(6):
+    orange_copy = cv2.pyrDown(orange_copy)
+    gp_orange.append(orange_copy)
 
+# generate Laplacian Pyramid for apple
+apple_copy = gp_apple[5]
+lp_apple = [apple_copy]
+for i in range(5, 0, -1):
+    gaussian_expanded = cv2.pyrUp(gp_apple[i])
+    laplacian = cv2.subtract(gp_apple[i-1], gaussian_expanded)
+    lp_apple.append(laplacian)
 
-# Load the images
-image1 = cv2.imread('apple.png')
-image2 = cv2.imread('orange.png')
-mask = cv2.imread('mask.png', 0)
+# generate Laplacian Pyramid for orange
+orange_copy = gp_orange[5]
+lp_orange = [orange_copy]
+for i in range(5, 0, -1):
+    gaussian_expanded = cv2.pyrUp(gp_orange[i])
+    laplacian = cv2.subtract(gp_orange[i-1], gaussian_expanded)
+    lp_orange.append(laplacian)
 
-# Normalize the mask to range [0, 1]
-mask = mask / 255.0
+# Now add left and right halves of images in each level
+apple_orange_pyramid = []
+n = 0
+for apple_lap, orange_lap in zip(lp_apple, lp_orange):
+    n += 1
+    cols, rows, ch = apple_lap.shape
+    laplacian = np.hstack((apple_lap[:, 0:int(cols/2)], orange_lap[:, int(cols/2):]))
+    apple_orange_pyramid.append(laplacian)
+# now reconstruct
+apple_orange_reconstruct = apple_orange_pyramid[0]
+for i in range(1, 6):
+    apple_orange_reconstruct = cv2.pyrUp(apple_orange_reconstruct)
+    apple_orange_reconstruct = cv2.add(apple_orange_pyramid[i], apple_orange_reconstruct)
 
-# Blend the images
-num_levels = 5
-blended_image = blend_images(image1, image2, mask, num_levels)
-
-# Display the blended image
-cv2.imshow('Blended Image', blended_image)
+cv2.imshow("apple", apple)
+cv2.imshow("orange", orange)
+cv2.imshow("apple_orange", apple_orange)
+cv2.imshow("apple_orange_reconstruct", apple_orange_reconstruct)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
