19db3d42ee5b0dad3f2d283d832a46d107b0d66d
nisaachar <thhat.guy.is@gmail.com>
2023-09-20 00:26:52 -0400

    some changes

--- Machine Learning/Assignment 1/exp.py
+++ Machine Learning/Assignment 1/exp.py
@@ -1,14 +1,5 @@
-## Code to run the decision tree on the Party dataset ##
-
-# Implemented by Stephen Marsland 
-# You are free to use, change, or redistribute the code in any way you wish for
-# non-commercial purposes, but please maintain the name of the original author.
-# This code comes with no warranty of any kind.
-
-
 import dtree
 import numpy as np
-
 
 def bootstrap_split(data, percentage=0.8):
     """Split the data into training and validation sets using bootstrap."""
@@ -18,40 +9,14 @@ def bootstrap_split(data, percentage=0.8):
     # Generate a bootstrap sample
     indices = np.random.choice(range(num_samples), size=train_size, replace=True)
     train_data = [data[i] for i in indices]
-        
+    
     # Validation set is the remaining data not included in the training set
-    validation_data = [data[i] for i in range(num_samples) if i not in indices]
+    validation_indices = [i for i in range(num_samples) if i not in indices]
+    validation_data = [data[i] for i in validation_indices]
     
     return train_data, validation_data
 
-
-# tree = dtree.dtree()
-# party,classes,features = tree.read_data('party.data')
-# t=tree.make_tree(party,classes,features)
-# # tree.printTree(t,' ')
-
-# # print(tree.classifyAll(t,party))
-
-# # for i in range(len(party)):
-# #     tree.classify(t,party[i])
-
-# # print("True Classes")
-# # print(classes)
-
-# true_classes = classes
-# predicted_classes = tree.classifyAll(t, party)
-
-# # Print true and predicted class labels for each instance
-# print("Instance\tTrue Class\tPredicted Class")
-# for i in range(len(party)):
-#     print(f"{party[i]}\t{true_classes[i]}\t\t{predicted_classes[i]}")
-
-# # Calculate training error rate
-# error_count = sum(1 for true_class, predicted_class in zip(true_classes, predicted_classes) if true_class != predicted_class)
-# training_error_rate = error_count / len(true_classes)
-
-# print("\nTraining Error Rate:", training_error_rate)
-
+# Create a decision tree instance
 tree = dtree.dtree()
 
 # Read the data
@@ -70,6 +35,7 @@ true_classes = list(validation_classes)
 predicted_classes = tree.classifyAll(t, list(validation_party))
 
 # Print the count of instances in the validation set
+print(f"Number of instances in the training set: {len(training_party)}")
 print(f"Number of instances in the validation set: {len(validation_party)}")
 
 # Print true and predicted class labels for each instance in the validation set
